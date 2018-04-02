1879b89535fbe242c01b0325d4bec866e180e5e9
Olivares <o.castillojl@gmail.com>
2018-04-02 00:22:51 -0500

    update utils

--- utils.py
+++ utils.py
@@ -1,23 +1,26 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import gzip
+from zipfile import ZipFile
+from io import BufferedReader
 
 __author__ = "Olivares Castillo José Luis"
 
 
-def read(file,vocabulary=None,is_zipped=True,dtype=np.float32):
+def read(file,vocabulary=None,is_zipped=True,encoding="utf-8",dtype=np.float64,):
     
     words = []
     
     if is_zipped:
-        header = file.readline().strip().decode().split()
+        header = file.readline().strip().decode(encoding).split()
         
     count = int(header[0]) if is_zipped else 5000
     dim = int(header[1]) if is_zipped else 300
     matrix = np.empty((count,dim), dtype=dtype) if vocabulary is None else []
     for i in range(count):
         if is_zipped:
-            word, vec = file.readline().decode().split(" ", 1)
+            word, vec = file.readline().decode(encoding).split(" ", 1)
         else:
             try:
                 word, vec = file.readline().split(" ", 1)
@@ -31,3 +34,67 @@ def read(file,vocabulary=None,is_zipped=True,dtype=np.float32):
             matrix.append(np.fromstring(vec,sep=" ", dtype=dtype))
     file.close()
     return (words,matrix) if vocabulary is None else (words,np.array(matrix,dtype=dtype))
+
+    
+def closest_word_to(top_10, words):
+    return [words[index] for index, _ in top_10]
+    
+    
+def get_lexicon(source, path=None):
+    path = "datasets/dictionaries/" if path is None else path
+    if source.__eq__("en-it.train"):
+        src,trg = load_lexicon(path+"en-it.train.drive.txt")
+    elif source.__eq__("en-it.test"):
+        src,trg = load_lexicon(path+"en-it.test.drive.txt")
+    elif source.__eq__("en-de.test"):
+        src,trg = load_lexicon(path+"en-de.test.txt")
+    elif source.__eq__("en-de.train"):
+        src,trg = load_lexicon(path+"en-de.train.txt")
+    elif source.__eq__("en-es.test"):
+        src,trg = load_lexicon(path+"en-es.test.txt")
+    elif source.__eq__("en-es.train"):
+        src,trg = load_lexicon(path+"en-es.train.txt")
+    elif source.__eq__("en-fi.test"):
+        src,trg = load_lexicon(path+"en-fi.test.txt")
+    elif source.__eq__("en-fi.train"):
+        src,trg = load_lexicon(path+"en-fi.train.txt")
+    else:
+        raise ValueError("{} no encontrado".format(source))
+    return (src,trg)
+    
+
+def load_lexicon(source):
+    src, trg = [], []
+    with open(source, "r") as file:
+        for line in file:
+            src.append(line.split()[0])
+            trg.append(line.split()[1])
+    return (src, trg)
+
+
+def open_file(source):
+    if source.__eq__("en-wiki"):
+        file = ZipFile("datasets/wiki-news-300d-1M-subword.vec.zip")\
+                        .open("wiki-news-300d-1M-subword.vec")
+    if source.__eq__("en-crawl"):
+        file = ZipFile("datasets/crawl-300d-2M.vec.zip")\
+                        .open("crawl-300d-2M.vec")
+    elif source.__eq__("it"):
+        file = gzip.open("datasets/en-it/cc.it.300.vec.gz",)
+    elif source.__eq__("de"):
+        file = gzip.open("datasets/en-de/cc.de.300.vec.gz")
+    elif source.__eq__("fi"):
+        file = gzip.open("datasets/en-fi/cc.fi.300.vec.gz")
+    elif source.__eq__("es"):
+        file = gzip.open("datasets/en-es/cc.es.300.vec.gz")
+    else:
+        raise ValueError("{} no encontrado".format(source))
+    return BufferedReader(file)
+    
+    
+def get_vectors(lexicon, words, embeddings, dtype='float'):
+    matrix = np.empty((len(lexicon), embeddings.shape[1]), dtype=dtype)
+    for i in range(len(lexicon)):
+        if lexicon[i] in words:
+            matrix[i] = embeddings[words.index(lexicon[i])]
+    return np.asarray(matrix, dtype=dtype)
